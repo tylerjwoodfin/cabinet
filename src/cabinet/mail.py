ff72a312a99b931e9f084cebf52c75b58a1e3282
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2023-04-16 11:52:22 -0500

    Implemented cli for mail

--- src/cabinet/mail.py
+++ src/cabinet/mail.py
@@ -15,74 +15,93 @@ import pwd
 import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from cabinet import Cabinet
+import cabinet
 
-cab = Cabinet()
-
-userDir = pwd.getpwuid(os.getuid())[0]
-
-# parameters
-port = cab.get("email", "port")
-smtp_server = cab.get("email", "smtp_server")
-imap_server = cab.get("email", "imap_server")
-username = cab.get("email", "from")
-password = cab.get("email", "from_pw")
-
-
-def send(subject: str, body: str, signature: str = '', to_addr: List[str] = None,
-         from_name: str = None, logging_enabled: bool = True, is_quiet: bool = False) -> None:
+class Mail:
     """
-    Sends an email with the given subject and body to the specified recipients.
-
-    Args:
-    - subject (str): The subject of the email.
-    - body (str): The body of the email.
-    - signature (str): The signature to include at the end of the email.
-    - to_addr (List): A list of email addresses to send the email to.
-    - from_name (str, optional): The name to appear in the "From" field of the email.
-        Reads from settings.json -> email -> from_name if unset.
-        If this is unset, defaults to `Raspberry Pi`
-    - logging_enabled (bool, optional): Whether to log the email send event.
-        Defaults to True.
-    - is_quiet: Whether to suppress log output.
-        Defaults to False.
-
-    Gmail will almost certainly not work.
+    Provides functionality for sending email using SMTP and MIMEText.
+    Does not support Gmail.
+    A throwaway email is highly recommended.
     """
 
-    from_name = (cab.get("email", "from_name") or "Raspberry Pi") + f" <{username}>"
-    signature = signature or f"<br><br>Thanks,<br>{from_name}"
+    def __init__(self):
+        self.user_dir = pwd.getpwuid(os.getuid())[0]
+        self.cab = cabinet.Cabinet()
 
-    if to_addr is None:
-        to_addr = cab.get("email", "to")
+        # parameters
+        self.port = self.cab.get("email", "port")
+        self.smtp_server = self.cab.get("email", "smtp_server")
+        self.imap_server = self.cab.get("email", "imap_server")
+        self.username = self.cab.get("email", "from")
+        self.password = self.cab.get("email", "from_pw")
 
+    def send(self, subject: str, body: str, signature: str = '', to_addr: List[str] = None,
+             from_name: str = None, logging_enabled: bool = True, is_quiet: bool = False) -> None:
+        """
+        Sends an email with the given subject and body to the specified recipients.
+
+        Args:
+        - subject (str): The subject of the email.
+        - body (str): The body of the email.
+        - signature (str): The signature to include at the end of the email.
+        - to_addr (List): A list of email addresses to send the email to.
+        - from_name (str, optional): The name to appear in the "From" field of the email.
+            Reads from settings.json -> email -> from_name if unset.
+            If this is unset, defaults to `Raspberry Pi`
+        - logging_enabled (bool, optional): Whether to log the email send event.
+            Defaults to True.
+        - is_quiet: Whether to suppress log output.
+            Defaults to False.
+
+        Raises:
+        - smtplib.SMTPAuthenticationError: If the SMTP server rejects the login credentials.
+
+        Gmail will almost certainly not work.
+        """
+
+        # Set default `from_name` if unset.
+        if from_name is None:
+            from_name = (self.cab.get("email", "from_name")
+                         or "Raspberry Pi") + f" <{self.username}>"
+
+        # Set default `to_addr` if unset.
         if to_addr is None:
-            cab.log("cabinet -> email -> to is unset", level="error")
-            return
+            to_addr = self.cab.get("email", "to")
 
-    # Parse
-    body += unquote(signature)
-    message = MIMEMultipart()
-    message["Subject"] = unquote(subject)
-    message["From"] = from_name
-    message["To"] = (', ').join(to_addr)
-    message.attach(MIMEText(unquote(body), "html"))
+            if to_addr is None:
+                self.cab.log("cabinet -> email -> to is unset", level="error")
+                return
 
-    # Send Email
-    server = smtplib.SMTP_SSL(smtp_server, port)
+        # Append `signature` to the `body` of the email.
+        signature = signature or f"<br><br>Thanks,<br>{from_name}"
+        body += unquote(signature)
 
-    try:
-        server.login(username, password)
-        server.send_message(message)
+        # Create the message object.
+        message = MIMEMultipart()
+        message["Subject"] = unquote(subject)
+        message["From"] = from_name
+        message["To"] = (', ').join(to_addr)
+        message.attach(MIMEText(unquote(body), "html"))
 
-        if logging_enabled:
-            cab.log(
-                f"Sent Email to {message['To']} as {message['From']}: {subject}", is_quiet=is_quiet)
+        # Send the email.
+        server = smtplib.SMTP_SSL(self.smtp_server, self.port)
 
-    except smtplib.SMTPAuthenticationError as err:
-        cab.log(
-            f"Could not log into {username}; set this with Cabinet.\n\n{err}", level="error")
+        try:
+            server.login(self.username, self.password)
+
+            server.send_message(message)
+
+            if logging_enabled:
+                self.cab.log(
+                    f"Sent Email to {message['To']} as {message['From']}: {subject}",
+                    is_quiet=is_quiet)
+
+        except smtplib.SMTPAuthenticationError as err:
+            self.cab.log(
+                f"Could not log into {self.username}; set this with Cabinet.\n\n{err}",
+                level="error")
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("sys.argv usage: send <subject>, <body>")
+        print("Usage: cabinet --mail -s <subject> -b <body> --to <to, optional>")
